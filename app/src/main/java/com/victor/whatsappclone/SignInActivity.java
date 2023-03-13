e2e7f8e94f8ff5bab5ca4309cc646267a3b92c19
Manuvel Victor <81816979+ManuvelVictor@users.noreply.github.com>
2023-03-13 21:10:54 +0530

    Implemented the login functionality to the sign In activity

--- app/src/main/java/com/victor/whatsappclone/SignInActivity.java
+++ app/src/main/java/com/victor/whatsappclone/SignInActivity.java
@@ -1,16 +1,59 @@
 package com.victor.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.victor.whatsappclone.databinding.ActivitySignInBinding;
+import com.victor.whatsappclone.databinding.ActivitySignUpBinding;
 
 public class SignInActivity extends AppCompatActivity {
 
+    ActivitySignInBinding binding;
+    FirebaseAuth mAuth;
+    FirebaseDatabase firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
         getSupportActionBar().hide();
+
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        binding.btnSignIn.setOnClickListener(v -> {
+            if (!binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty()) {
+                binding.progressBar.setVisibility(View.VISIBLE);
+                mAuth.signInWithEmailAndPassword(binding.txtEmail.getText().toString(), binding.txtPassword.getText().toString()).addOnCompleteListener(task -> {
+                    binding.progressBar.setVisibility(View.GONE);
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(SignInActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            } else {
+                Toast.makeText(SignInActivity.this, "Enter Credentials", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        if (mAuth.getCurrentUser() != null) {
+//            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+//            startActivity(intent);
+//        }
     }
 }
