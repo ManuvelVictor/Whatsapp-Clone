cd84377977170312b3f287354f7c8e417589b0c9
Manuvel Victor <81816979+ManuvelVictor@users.noreply.github.com>
2023-03-14 09:43:46 +0530

    Created the menu option for settings, groupchat and logout implentend the logout functionality implemented the google sign in option in SignInActivity

--- app/src/main/java/com/victor/whatsappclone/SignInActivity.java
+++ app/src/main/java/com/victor/whatsappclone/SignInActivity.java
@@ -1,38 +1,64 @@
 package com.victor.whatsappclone;
 
-import androidx.annotation.NonNull;
+import static com.google.android.gms.auth.api.signin.GoogleSignIn.getClient;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.AuthCredential;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 import com.victor.whatsappclone.databinding.ActivitySignInBinding;
-import com.victor.whatsappclone.databinding.ActivitySignUpBinding;
+import com.victor.whatsappclone.model.Users;
+
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 public class SignInActivity extends AppCompatActivity {
 
     ActivitySignInBinding binding;
     FirebaseAuth mAuth;
     FirebaseDatabase firebaseDatabase;
+
+    GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         mAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = getClient(this, gso);
+
 
         binding.btnSignIn.setOnClickListener(v -> {
             if (!binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty()) {
@@ -43,7 +69,7 @@ public class SignInActivity extends AppCompatActivity {
                         Intent intent = new Intent(SignInActivity.this, MainActivity.class);
                         startActivity(intent);
                     } else {
-                        Toast.makeText(SignInActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(SignInActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
             } else {
@@ -51,9 +77,57 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
-//        if (mAuth.getCurrentUser() != null) {
-//            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
-//            startActivity(intent);
-//        }
+        if (mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
+        binding.btnGoogle.setOnClickListener(v -> signIn());
+    }
+
+    int RC_SIGN_IN = 123;
+
+    private void signIn() {
+        Intent signInIntent = new Intent(mGoogleSignInClient.getSignInIntent());
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.w("TAG", "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d("TAG", "signInWithCredential:success ");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        Users users = new Users();
+                        assert user != null;
+                        users.setUserId(user.getUid());
+                        users.setUserName(user.getDisplayName());
+                        users.setProfilePic(Objects.requireNonNull(user.getPhotoUrl()).toString());
+                        firebaseDatabase.getReference().child("Users").child(user.getUid()).setValue(users);
+
+                        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(SignInActivity.this, "Sign In with Google", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.w("TAG", "SignInWithCredential:failure", task.getException());
+                    }
+                });
     }
 }
